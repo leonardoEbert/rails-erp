90f4354e832855aabfae16be0ba0ac5630539252
Leonardo Ebert <leoebert19@gmail.com>
2022-07-14 00:27:57 -0300

    feat: updated database schema

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_11_041632) do
     t.string "first_name"
     t.string "last_name"
     t.date "birth_date"
+    t.boolean "active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
